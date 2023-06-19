d6d2f074aaca08283db2672d3d1e8de60af456c5
am230 <111672334+am230@users.noreply.github.com>
2023-06-19 18:51:26 +0900

    Refactor: index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Form } from 'react-final-form';
-import { Response } from "./api/channel_id";
+import React, {useState} from 'react';
+import {Form} from 'react-final-form';
+import {Response} from "./api/channel_id";
 import axios from "axios";
 
 const Home = () => {
@@ -13,18 +13,16 @@ const Home = () => {
     };
 
     const createLink = async () => {
-        const res = await axios.post<Response>('/api/channel_id', { url });
+        const res = await axios.post<Response>('/api/channel_id', {url});
         const response = res.data;
         destroyMessage();
 
         if (!response.ok) {
             console.log(response);
-            setMessage(
-                <p className='message failed'>
-                    <span className="material-symbols-outlined">&#xe000;</span>
-                    {response.error}
-                </p>
-            );
+            setMessage(<p className='message failed'>
+                <span className="material-symbols-outlined">&#xe000;</span>
+                {response.error}
+            </p>);
             setTimeout(() => destroyMessage(), 5000);
             return;
         }
@@ -32,22 +30,18 @@ const Home = () => {
         const result = `${window.location.origin}/api/redirect/${response.id}`;
         setResult(result);
         await navigator.clipboard.writeText(result);
-        setMessage(
-            <p className='message'>
-                <span className="material-symbols-outlined">&#xe86c;</span>
-                Copied!
-            </p>
-        );
+        setMessage(<p className='message'>
+            <span className="material-symbols-outlined">&#xe86c;</span>
+            Copied!
+        </p>);
         setTimeout(() => destroyMessage(), 5000);
     };
 
-    return (
-        <div className='main'>
-            <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    return (<div className='main'>
+            <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet"/>
             <Form
                 onSubmit={createLink}
-                render={({ handleSubmit }) => (
-                    <form onSubmit={handleSubmit}>
+                render={({handleSubmit}) => (<form onSubmit={handleSubmit}>
                         <input
                             type="text"
                             placeholder='@id'
@@ -55,12 +49,14 @@ const Home = () => {
                             onChange={(e) => setUrl(e.target.value)}
                         />
                         <button onClick={handleSubmit}>Create & Copy</button>
-                    </form>
-                )}
+                    </form>)}
             />
             {message}
-        </div>
-    );
+            <div className={"bottom"}>
+                <a href="https://github.com/am230/youtube-live-chat-redirect">YTChat Redirect</a>
+                Made by <a href="https://twitter.com/AM4_02">二時半</a>
+            </div>
+        </div>);
 };
 
 export default Home;
